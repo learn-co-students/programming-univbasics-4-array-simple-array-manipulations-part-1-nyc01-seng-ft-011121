6516a33edfc39c20bf0640627adc02359822b848
Gabriel Demes <gabrieldemes@nyu.edu>
2020-11-30 21:23:32 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(list, string)
+  list.push(string)
+end
+
+def using_unshift(list, string)
+  list.unshift(string)
+end
+
+def using_pop(list)
+  list.pop
+end
+
+def pop_with_args(list)
+  list.pop(2)
+end
+
+def using_shift(list)
+  list.shift
+end
+
+def shift_with_args(list)
+  list.shift(2)
+end
